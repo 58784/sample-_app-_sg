4258b061dfdb06019a58354f589b42569c4e9332
58784 <m7mkmnn@gmail.com>
2023-01-22 07:59:26 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,12 +2,32 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token # セッションリプレイ攻撃から保護する
   end
   
-  # 現在ログイン中のユーザーを返す（いる場合）
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  # permanent = 永続的（20年）
+  # encrypted = 暗号化
+  def remember(user)
+    user.remember #モデルで定義したremember。記憶トークンを作成、ハッシュ化してDBに保存
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+      # if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -16,8 +36,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget #モデルで定義したforget。DBの記憶ダイジェストにnilを登録。
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     reset_session
     @currenr_user = nil  # 安全のため
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  before_save { email.downcase!}
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   validates :name,  presence: true, length: {maximum: 50 }
@@ -15,5 +16,36 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためにユーザーをDBに記憶する
+  def remember
+    self.remember_token = User.new_token # rememberのtoken(ランダム文字列)として、新しく作成してる
+    update_attribute(:remember_digest, User.digest(remember_token)) # 作成後、tokenをdigest化（ハッシュ化）して:remember_digestとしてカラムに保存
+    remember_digest  
+  end
+  
+  # セッションハイジャック防止のためにセッショントークンを返す
+  # この記憶ダイジェストを再利用しているのは単に利便性のため
+  def session_token
+    remember_digest || remember
+  end
+  
+  # 暗号化された文字列(remember_digest)が
+  # 検証したい文字列（remember_token）から暗号化する文字列と
+  # 一致するかを検証している
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーの永続ログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end

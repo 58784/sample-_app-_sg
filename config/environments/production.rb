749e095bc94ef96a0416c2126400c315d64195dc
58784 <m7mkmnn@gmail.com>
2023-01-24 08:00:46 +0000

    change host name by production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sandbox8ff9ffe94b854047ba8492f219a6e17d.mailgun.org.onrender.com'
+  host = 'sample_app.mailgun.org.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,

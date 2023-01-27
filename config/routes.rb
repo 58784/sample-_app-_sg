4c9a0a066b2588c77d8ea15a277f6ab97ea28eb9
58784 <m7mkmnn@gmail.com>
2023-01-27 06:52:15 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,14 @@ Rails.application.routes.draw do
   get "login",    to: "sessions#new"
   post "login",   to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: %w[edit]
   resources :password_resets , only: %w[new create edit update]
-  resources :microposts, only: %w[ create destroy]
+  resources :microposts, only: %w[ create destroy ]
+  resources :relationships, only: %w[ create destroy ]
   get '/microposts', to: 'static_pages#home'
 end
